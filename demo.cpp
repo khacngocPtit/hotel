d4a647cd76a502de5cc639b34a1d6323672bc622
khacngocPtit <khacngocit@gmail.com>
2019-11-03 11:46:29 +0700

    ...

--- demo.cpp
+++ demo.cpp
@@ -4,12 +4,31 @@ using namespace std;
 
 class Nguoi{
 	protected:
-		string name[30];
-		string email[100];
+		char name[30];
+		char email[100];
 		sdt string[11];
 	public:
 		void nhap();
 		void hienThi();
 };
-
+class khachHang :public Nguoi{
+	
+	private:
+		char maKhachHang[30];
+		int kieuPhong;
+		char moTa[100];
+	public:
+		void nhap();
+		void hienThi();
+};
+class Phong{
+	private:
+		char maPhong[30];
+		int kieuPhong;
+		float mucTienThue;
+	public:
+		void nhap();
+		void hienThi();
+};
+void bangSapXep()
 
